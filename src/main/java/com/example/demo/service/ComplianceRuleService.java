58aa216afd4fcb2181957993043579395e037920
bala-amypo <bala@amypo.in>
2025-12-18 10:02:09 +0000

    User commit

--- src/main/java/com/example/demo/service/ComplianceRuleService.java
+++ src/main/java/com/example/demo/service/ComplianceRuleService.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 public interface ComplianceRuleService {
 
-    // REQUIRED to fix "cannot find symbol save(...)"
+    // Used by controllers/tests
     ComplianceRule save(ComplianceRule rule);
 
     ComplianceRule createRule(ComplianceRule rule);
+
+    // REQUIRED to fix "cannot find symbol findAll()"
+    List<ComplianceRule> findAll();
 
     List<ComplianceRule> getAllRules();
 
