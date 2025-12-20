fe6553fa9c1583d21b88f0d165c8a9b9733d16ec
bala-amypo <bala@amypo.in>
2025-12-20 14:46:53 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,9 +1,10 @@
 package com.example.demo.service;
 
+import com.example.demo.exception.ValidationException;
 import com.example.demo.model.User;
 
 public interface UserService {
-    User registerUser(User user);
+    User register(User user) throws ValidationException; // Add this method
     User findByEmail(String email);
-    User getById(Long id);
+    User getUser(Long id);
 }
