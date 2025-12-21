8b4f4262e012fda65442738e34325e0b543114d5
bala-amypo <bala@amypo.in>
2025-12-21 10:25:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/DocumentTypeRepository.java
+++
@@ -1,9 +0,0 @@
-// com.example.demo.repository.DocumentTypeRepository.java
-package com.example.demo.repository;
-
-import com.example.demo.model.DocumentType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentTypeRepository extends JpaRepository<DocumentType, Long> {
-    boolean existsByTypeName(String typeName);
-}
