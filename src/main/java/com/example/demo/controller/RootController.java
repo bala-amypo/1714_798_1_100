a4d0252a6ccd4172ec9984cd5e97acbebe2d4dd1
bala-amypo <bala@amypo.in>
2025-12-21 05:39:02 +0000

    User commit

--- src/main/java/com/example/demo/controller/RootController.java
+++ src/main/java/com/example/demo/controller/RootController.java
@@ -1,13 +1,13 @@
-package com.example.demo.controller;
+// package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class RootController {
+// @RestController
+// public class RootController {
     
-    @GetMapping("/")
-    public String home() {
-        return "Vendor Compliance API is running. Go to /swagger-ui/index.html for API documentation.";
-    }
-}
+//     @GetMapping("/")
+//     public String home() {
+//         return "Vendor Compliance API is running. Go to /swagger-ui/index.html for API documentation.";
+//     }
+// }
