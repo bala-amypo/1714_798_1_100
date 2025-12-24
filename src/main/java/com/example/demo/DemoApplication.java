2522b5e3b013c37df1066e154f59908a2967f9e1
bala-amypo <bala@amypo.in>
2025-12-24 08:19:06 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,20 +12,38 @@
 // }
 
 
+// package com.example.demo;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication(
+// 	 exclude = {
+//         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+//     }
+// )
+// public class DemoApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(DemoApplication.class, args);
+// 	}
+
+// }
+
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication(
-	 exclude = {
+    exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
     }
 )
+@ServletComponentScan
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
