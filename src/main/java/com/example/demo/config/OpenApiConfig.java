aec52e8238e4df3f551ded54193d978af559bd96
bala-amypo <bala@amypo.in>
2025-12-30 04:05:14 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -38,7 +38,7 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Vendor Compliance")
+                        .title("Digital Credentia")
                         .version("1.0.0")
                         .description("Digital Credential Verification Engine")
                         .contact(new Contact()
